dcc8df57e6afb756235809ada51bc39c97b9a630
Jmaes-Nag <mezng13@byu.edu>
2026-08-27 22:39:18 -0600

    check that API key doesn't start with placeholder so `sed` doesn't break condition

--- frontend/app.js
+++ frontend/app.js
@@ -88,7 +88,8 @@ function updateMapTiles() {
     
     // Choose CartoDB Dark Matter or Positron (light mode)
     const cartoApiKey = '__CARTO_API_KEY__';
-    const apiKeyParam = (cartoApiKey && cartoApiKey !== '__CARTO_API_KEY__') ? `?api_key=${cartoApiKey}` : '';
+    // Check that it doesn't start with the placeholder syntax so `sed` doesn't break our condition
+    const apiKeyParam = (cartoApiKey && !cartoApiKey.startsWith('__CARTO')) ? `?api_key=${cartoApiKey}` : '';
     
     const url = state.theme === "dark" 
         ? `https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png${apiKeyParam}`
